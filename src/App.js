8e2685716f19ea8d9323d9f4fc28e60756131605
Monirul-RS <monir030599@gmail.com>
2022-11-08 01:51:45 +0600

    setup second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from './Routers/Routes/Routes';
 function App() {
 
   return (
-    <div>
+    <div className='max-w-screen-2xl mx-auto'>
       <RouterProvider router={router}></RouterProvider>    
     </div>
   );
