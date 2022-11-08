297b98b081da165eb10c08a1487481a3cecf6346
Monirul-RS <monir030599@gmail.com>
2022-11-09 01:04:38 +0600

    banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import pic1 from '../../../assets/images/Banner/pic1.jpg';
+import pic2 from '../../../assets/images/Banner/pic2.jpg';
+import pic3 from '../../../assets/images/Banner/pic3.jpg';
+import pic4 from '../../../assets/images/Banner/pic4.jpg';
+import pic5 from '../../../assets/images/Banner/pic5.jpg';
+import pic6 from '../../../assets/images/Banner/pic6.jpg';
+
 import './Banner.css'
 import BannerItem from './BannerItem';
 
@@ -7,37 +14,37 @@ const Banner = () => {
 
     const bannerData = [
         {
-            image: 1,
+            image: pic1,
             prev: 6,
             id: 1,
             next: 2
         },
         {
-            image: 2,
+            image: pic2,
             prev: 1,
             id: 2,
             next: 3
         },
         {
-            image: 3,
+            image: pic3,
             prev: 2,
             id: 3,
             next: 4
         },
         {
-            image: 4,
+            image: pic4,
             prev: 3,
             id: 4,
             next: 5
         },
         {
-            image: 5,
+            image: pic5,
             prev: 4,
             id: 5,
             next: 6
         },
         {
-            image: 6,
+            image: pic6,
             prev: 5,
             id: 6,
             next: 1
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
 
     </>
     return (
-        <div className="navbar h-24 mb-12 bg-base-200">
+        <div className="navbar h-24 rounded-xl bg-base-200">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
